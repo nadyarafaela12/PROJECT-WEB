e5b4b9194e1ffc7545746c53cb845b251fe353b0
Dimas Wahyudi <91781790+DimasLdw@users.noreply.github.com>
2022-11-24 22:34:56 +0700

    Add files via upload

---
+++ Laporan/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+mysqli_connect($koneksi, "localhost","root","") or die("");
+mysqli_select_db($koneksi, "db_sipeg") or die("");
+
+// $link = mysqli_connect("localhost","username","password","database") or die("Error " . mysqli_error($link));
+?>
+
